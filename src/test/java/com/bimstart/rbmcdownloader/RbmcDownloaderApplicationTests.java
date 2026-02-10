a3e3f45c3a2433460531fcfb26feb1706d4eae77
Bruno Rodrigues Coutinho <brunorfcoutinho@gmail.com>
2026-02-10 19:52:35 -0300

    primeiro commit

---
+++ src/test/java/com/bimstart/rbmcdownloader/RbmcDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bimstart.rbmcdownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbmcDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
