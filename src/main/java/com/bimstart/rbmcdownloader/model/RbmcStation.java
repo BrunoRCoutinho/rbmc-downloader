4998e6ff45383a1dca5c069b0eb78c5b0902f783
Bruno Rodrigues Coutinho <brunorfcoutinho@gmail.com>
2026-03-22 17:04:11 -0300

    Att Localizador

--- src/main/java/com/bimstart/rbmcdownloader/model/RbmcStation.java
+++ src/main/java/com/bimstart/rbmcdownloader/model/RbmcStation.java
@@ -2,13 +2,14 @@ package com.bimstart.rbmcdownloader.model;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class RbmcStation {
-
     private String codigo;
     private double latitude;
     private double longitude;
     private String estado;
+    private List<String> intervalo;
 }
